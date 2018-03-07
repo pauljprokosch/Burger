1dd3f7b9e02d10b93ee6144fe1d1979a9e0820ca
Paul Prokosch <paul.prokosch@gmail.com>
2018-03-06 20:14:29 -0600

    implemented 99% of capapabilities of what should occure when devour button is pressed

--- models/burger.js
+++ models/burger.js
@@ -21,10 +21,10 @@ $("#submit-button").on("click", function(event) {
      //creating html elements for the data that the user submitted
       var row = $("<div>");
       row.addClass("burger");
-      row.append("<p>" + newBurger.burgerName + "</p>");
-      row.append("<button id= devour-button>Devour!</button>");
+      row.append("<p id='burger-text'>" + newBurger.burgerName + "</p>");
+      row.append("<button id= 'devour-button'>Devour!</button>");
   	//using jquery to send our newly created burger to the devour section of our web page
-      $("#devour").append(row);
+      $("#2bdevoured").append(row);
      //Insert a new burger into the burger table
 	  orm.insertOne(newBurger.burgerName,burgers);
     });
@@ -32,4 +32,20 @@ $("#submit-button").on("click", function(event) {
   // Empty each input box by replacing the value with an empty string
   $("#burger-name-input").val("");
 });
+
+$("#2bdevoured").on("click", "devour-button", function(event){
+	//we want to store the information from the burger before it was devoured (burgername)
+	//TODO:  reference the row div that was set earlier
+	var doneDevoured = $(this).val().trim();
+	//We want to get rid of the old html that was containing that burger in the section on the left.
+	this.empty();
+	//we want to create a new html element in the section where new burgers that are devoured should appear
+	 var row = $("<div>");
+      row.addClass("burger");
+      row.append("<p id='burger-text'>" + doneDevoured + "</p>");
+  	//using jquery to send our newly created burger to the devour section of our web page
+      $("#devoured").append(row);
+     //Update the flag of the burger to devoured
+	  orm.updateOne(burgers,"devoured","burgerName",doneDevoured);
+})
 
